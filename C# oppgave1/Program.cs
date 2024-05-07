60d4f4e8ca91465cb900041ffa90c87b289f23b3
EspenB08 <142892271+EspenB08@users.noreply.github.com>
2024-05-07 09:30:26 +0200

    Add project files.

---
+++ C# oppgave1/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+
+
+
+
+
+int myNum = 5;
+double myDoubleNum = 10.92F;
+string myText = "kekmek var her";
+
+Console.WriteLine(myText);
+Console.WriteLine(myDoubleNum);
+Console.WriteLine(myNum);
+
+
+string[] midLaner = { "Sylas", "Syndra", "Xerath", "Azir", "Corki", "Ziggs" };
+//Console.WriteLine(midLaner[0]);
+
+for (int i = 0; i < midLaner.Length; i++)
+    Console.WriteLine(midLaner[i]);
+
+List<string> Jungler = new List<string>();
+
+Jungler.Add("Graves");
+Jungler.Add("Hecarim");
+Jungler.Add("Evelynn");
+Jungler.Add("Kha'zix");
+Jungler.Add("Lee sin");
+Jungler.Add("Elise");
+
+Jungler.Remove("Hecarim");
+foreach (string s in Jungler)
+    Console.WriteLine(s);
+
+Adc adc = new Adc("Utility", "Ashe");
+Adc adc1 = new Adc("Dps", "Kog'Maw");
+Adc adc2 = new Adc("Dps", "Draven");
+
+
+adc.GetAdc();
+adc1.GetAdc();
+adc2.GetAdc();
---
+++ C# oppgave1/testConstructor.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Data;
+
+public class Adc
+{
+    public string? Typing { get; set; }
+    public string? Name { get; set; }
+
+ 
+public Adc(string? typing, string? name)
+{
+    Typing = typing;
+    Name = name;
+
+}
+
+public void GetAdc()
+{
+       
+    Console.WriteLine($"Champion name: {Name}, Champion class: {Typing}");
+}
+}
